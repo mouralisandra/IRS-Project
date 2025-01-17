d08efedeba66d7d866a97ffc6fc4ad001b027e99
Anas Chaibi <anas.chaibi@lighton.ai>
2025-01-17 16:38:49 +0100

    chore: edited ui & fixed nltk init

--- modules/preprocessingAndIndexing.py
+++ modules/preprocessingAndIndexing.py
@@ -1,8 +1,18 @@
 from collections import defaultdict
 import re
 import pandas as pd
+import ssl
 import nltk
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
 nltk.download('stopwords')
+
 nltk.download('wordnet')
 
 from nltk.corpus import stopwords
